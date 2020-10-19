5b6d3588266e5e253d9b40031c6edf2fbc98a267
anajavitti <a266010@dac.unicamp.br>
2020-10-19 10:18:29 -0300

    py

---
+++ python/mc102/lab07/resp/lab07t.py
@@ -0,0 +1,16 @@
+obj1 = ["Q", "T", "L", "H", "O"]
+
+obj = input()
+cara = input()
+dimen = int(input())
+ 
+ 
+if obj == obj1[1]: #se for tri^ngulo dimen = altura
+
+    k = int(dimen * 2 - 1) # n° de caracteres na ultima fileira
+    a = int(((dimen - 1) * 2 - 1)/2) # espaços 
+    b = 1
+    for i in range (dimen):
+      print (a * " ", cara * b, a * " ")
+      a -= 1
+      b = b + 2
